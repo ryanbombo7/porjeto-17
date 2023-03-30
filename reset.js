bc2d555744d6667e3f718a08ee5a0059080ab7c3
ryanbombo7 <126615122+ryanbombo7@users.noreply.github.com>
2023-03-30 15:20:42 -0300

    Add files via upload

---
+++ reset.js
@@ -0,0 +1,20 @@
+//function reset{
+//  gameState = END;
+//  gameOver.visible = false;
+//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+//  pinkCG.destroyEach();
+//  yellowCG.destroyEach();
+//  redCG.destroyEach();
+//  distance = 0;
+// }
+//function reset() {
+//  gameState = PLAY;
+//  gameOver.visible = true;
+//  mainCyclist.addAnimation("SahilRunning", mainRacerImg1);
+
+//  pinkCG.destroy();
+//  yellowCG.destroy();
+//  redCG.destroy();
+
+//  distance = 0;
+//}
